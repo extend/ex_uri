c79a0971844b00fc0bbdf530cfbd9a9e26a3fd0f
Anthony Ramine <nox@dev-extend.eu>
2010-12-17 02:18:32 +0100

    Fix Dializer record types

--- include/ex_uri.hrl
+++ include/ex_uri.hrl
@@ -1,14 +1,14 @@
--record(ex_uri_authority, {userinfo :: string(),
+-record(ex_uri_authority, {userinfo :: string() | undefined,
                            host :: string(),
-                           port :: integer()}).
+                           port :: integer() | undefined}).
 
 -record(ex_uri, {scheme :: string(),
-                 authority :: #ex_uri_authority{},
+                 authority :: #ex_uri_authority{} | undefined,
                  path :: [string()],
-                 q :: string(),
-                 fragment :: string()}).
+                 q :: string() | undefined,
+                 fragment :: string() | undefined}).
 
--record(ex_uri_ref, {authority :: #ex_uri_authority{},
+-record(ex_uri_ref, {authority :: #ex_uri_authority{} | undefined,
                      path :: [string()],
-                     q :: string(),
-                     fragment :: string()}).
+                     q :: string() | undefined,
+                     fragment :: string() | undefined}).
