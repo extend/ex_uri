419166ddbdf92e4adcd46788d2d2738aa8c458aa
Anthony Ramine <nox@dev-extend.eu>
2010-12-17 18:05:07 +0100

    Add resolve function

--- src/ex_uri.erl
+++ src/ex_uri.erl
@@ -37,7 +37,10 @@
          decode_abs/1,
          decode_pct/1,
          encode/1,
-         encode_pct/2]).
+         encode_pct/2,
+         resolve/2,
+         merge/2,
+         remove_dot_segments/1]).
 
 %% @spec decode(string()) -> {ok, uri(), string()}
 %% @doc Decode an URI.
@@ -113,6 +116,42 @@ encode_authority(#ex_uri_authority{userinfo = UserInfo,
     undefined -> Acc2;
     P -> [integer_to_list(P), $: | Acc2] end.
 
+%% @spec resolve(Ref :: ref(), Base :: uri()) -> uri()
+%% @doc Resolve an URI reference with a given base URI.
+resolve(#ex_uri_ref{authority = RA, path = RP, q = RQ, fragment = RF},
+        #ex_uri{scheme = BS, authority = BA, path = BP, q = BQ}) ->
+  [TA, TP, TQ] = case RA of
+                   undefined ->
+                     Acc = case RP of
+                             "" ->
+                               TQ1 = case RQ of
+                                       undefined -> BQ;
+                                       RQ -> RQ end,
+                               [BP, TQ1];
+                             RP ->
+                               TP1 = case RP of
+                                       [$/ | _] -> RP;
+                                       RP -> merge(RP, BP) end,
+                               [remove_dot_segments(TP1), RQ] end,
+                     [BA | Acc];
+                   RA ->
+                     [RA, remove_dot_segments(RP), RQ] end,
+  #ex_uri{scheme = BS, authority = TA, path = TP, q = TQ, fragment = RF};
+resolve(Ref = #ex_uri{path = RP}, #ex_uri{}) ->
+  Ref#ex_uri{path = remove_dot_segments(RP)}.
+
+%% @spec merge(string(), string()) -> string()
+%% @doc Merge a relative path with a given base path.
+merge(Rel, "") ->
+  "/" ++ Rel;
+merge(Rel, Base) ->
+  merge(Rel, Base, "", "").
+
+%% @spec remove_dot_segments(string()) -> string()
+%% @doc Remove superfluous dot segments from a given path.
+remove_dot_segments(Path) ->
+  remove_dot_segments1(Path).
+
 %% @hidden
 encode_pct([C | String], Chars, Acc) ->
   case lists:member(C, Chars) of
@@ -120,6 +159,39 @@ encode_pct([C | String], Chars, Acc) ->
       [D1, D2] = erlang:integer_to_list(C, 16),
       encode_pct(String, Chars, [D2, D1, $% | Acc]);
     false -> encode_pct(String, Chars, [C | Acc]) end.
+
+%% @hidden
+merge(Rel, [$/ | Base], Last, Acc) ->
+  merge(Rel, Base, [], [$/ | Last] ++ Acc);
+merge(Rel, [C | Base], Last, Acc) ->
+  merge(Rel, Base, [C | Last], Acc);
+merge(Rel, [], _Last, Acc) ->
+  lists:reverse(Acc, Rel).
+
+%% @hidden
+remove_dot_segments1("/" ++ Path) ->
+  remove_dot_segments1(Path, "/", "");
+remove_dot_segments1(Path) ->
+  remove_dot_segments1(Path, "", "").
+
+%% @hidden
+remove_dot_segments1("", Prefix, Acc) ->
+  Prefix ++ lists:reverse(Acc);
+remove_dot_segments1(Path, Prefix, Acc) ->
+  Pred = fun (C) -> C =/= $/ end,
+  {S, R} = case lists:splitwith(Pred, Path) of
+             {Segment, "/" ++ Rest} -> {Segment ++ "/", Rest};
+             Result -> Result end,
+  if
+    S =:= "."; S =:= "./" ->
+      remove_dot_segments1(R, Prefix, Acc);
+    S =:= ".."; S =:= "../" ->
+      Acc2 = case lists:dropwhile(Pred, Acc) of
+               "/" ++ Acc1 -> Acc1;
+               Acc1 -> Acc1 end,
+      remove_dot_segments1(R, Prefix, lists:dropwhile(Pred, Acc2));
+    true ->
+      remove_dot_segments1(R, Prefix, lists:reverse(S, Acc)) end.
 
 
 -ifdef(TEST).
@@ -150,3 +222,69 @@ encode_test_() ->
                fun () ->
                      String = encode(URI) end end,
   [ Test(String, URI) || {String, URI} <- simple_uris() ].
+
+%% @hidden
+resolve_test_() ->
+  BaseLit = "http://a/b/c/d;p?q",
+  Data = [% http://tools.ietf.org/html/rfc3986#section-5.4.1
+          {"g:h", BaseLit, "g:h"},
+          {"g", BaseLit, "http://a/b/c/g"},
+          {"./g", BaseLit, "http://a/b/c/g"},
+          {"g/", BaseLit, "http://a/b/c/g/"},
+          {"/g", BaseLit, "http://a/g"},
+          {"//g", BaseLit, "http://g"},
+          {"?y", BaseLit, "http://a/b/c/d;p?y"},
+          {"g?y", BaseLit, "http://a/b/c/g?y"},
+          {"#s", BaseLit, "http://a/b/c/d;p?q#s"},
+          {"g#s", BaseLit, "http://a/b/c/g#s"},
+          {"g?y#s", BaseLit, "http://a/b/c/g?y#s"},
+          {";x", BaseLit, "http://a/b/c/;x"},
+          {"g;x", BaseLit, "http://a/b/c/g;x"},
+          {"g;x?y#s", BaseLit, "http://a/b/c/g;x?y#s"},
+          {"", BaseLit, BaseLit},
+          {".", BaseLit, "http://a/b/c/"},
+          {"./", BaseLit, "http://a/b/c/"},
+          {"..", BaseLit, "http://a/b/"},
+          {"../", BaseLit, "http://a/b/"},
+          {"../g", BaseLit, "http://a/b/g"},
+          {"../..", BaseLit, "http://a/"},
+          {"../../", BaseLit, "http://a/"},
+          {"../../g", BaseLit, "http://a/g"},
+          % http://tools.ietf.org/html/rfc3986#section-5.4.2
+          {"../../../g", BaseLit, "http://a/g"},
+          {"../../../../g", BaseLit, "http://a/g"},
+          {"/./g", BaseLit, "http://a/g"},
+          {"/../g", BaseLit, "http://a/g"},
+          {"g.", BaseLit, "http://a/b/c/g."},
+          {".g", BaseLit, "http://a/b/c/.g"},
+          {"g..", BaseLit, "http://a/b/c/g.."},
+          {"..g", BaseLit, "http://a/b/c/..g"},
+          {"./../g", BaseLit, "http://a/b/g"},
+          {"./g/.", BaseLit, "http://a/b/c/g/"},
+          {"g/./h", BaseLit, "http://a/b/c/g/h"},
+          {"g/../h", BaseLit, "http://a/b/c/h"},
+          {"g;x=1/./y", BaseLit, "http://a/b/c/g;x=1/y"},
+          {"g;x=1/../y", BaseLit, "http://a/b/c/y"},
+          {"g?y/./x", BaseLit, "http://a/b/c/g?y/./x"},
+          {"g?y/../x", BaseLit, "http://a/b/c/g?y/../x"},
+          {"g#s/./x", BaseLit, "http://a/b/c/g#s/./x"},
+          {"g#s/../x", BaseLit, "http://a/b/c/g#s/../x"},
+          {"http:g", BaseLit, "http:g"},
+          % http://lists.w3.org/Archives/Public/uri/2004Feb/0114.html
+          {"../c", "foo:a/b", "foo:c"},
+          {"foo:.", "foo:a", "foo:"},
+          {"/foo/../../../bar", "zz:abc", "zz:/bar"},
+          {"/foo/../bar", "zz:abc", "zz:/bar"},
+          {"foo/../../../bar", "zz:abc", "zz:bar"},
+          {"foo/../bar", "zz:abc", "zz:bar"},
+          {"zz:.", "zz:abc", "zz:"},
+          {"/.", BaseLit, "http://a/"},
+          {"/.foo", BaseLit, "http://a/.foo"},
+          {".foo", BaseLit, "http://a/b/c/.foo"}],
+  Test = fun (R, B, T) ->
+               fun () ->
+                     {ok, Ref, ""} = ex_uri:decode_ref(R),
+                     {ok, Base, ""} = ex_uri:decode(B),
+                     Target = ex_uri:resolve(Ref, Base),
+                     T = ex_uri:encode(Target) end end,
+  [ Test(R, B, T) || {R, B, T} <- Data ].
